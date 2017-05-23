d9e3bfca1db90859679f083318433ab3fd008d6f
Stefan Botzenhart <sb@basiszwo.com>
2017-05-23 12:40:12 +0200

    Adjust access modifier

--- src/main/java/one/flak/example/api/Main.java
+++ src/main/java/one/flak/example/api/Main.java
@@ -28,7 +28,7 @@ public class Main {
         });
     }
 
-    public static void setupKafka(String [] args) {
+    private static void setupKafka(String [] args) {
         checkArguments(args);
 
         Properties props = new Properties();
@@ -42,14 +42,14 @@ public class Main {
         kafkaProducer = new KafkaProducer<String, String>(props);
     }
 
-    public static void showHelp() {
+    private static void showHelp() {
         System.out.println("API Endpoint to accept json requests");
         System.out.println("");
         System.out.println("Usage:");
         System.out.println("java -jar api.jar configuration.properties");
     }
 
-    public static void checkArguments(String [] args) {
+    private static void checkArguments(String [] args) {
         if(args.length < 1 || args[0].equals("-h") || args[0].equals("--help")) {
             showHelp();
             System.exit(0);
